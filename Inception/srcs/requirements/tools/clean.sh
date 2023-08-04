581fbeb2a7150ea52247e2de7867a0c0f0dc4951
marco paternosto frosi <marco@MacBook-Air-di-marco-2.local>
2023-08-04 14:23:03 +0200

    fixes

--- Inception/srcs/requirements/tools/clean.sh
+++ Inception/srcs/requirements/tools/clean.sh
@@ -1,7 +1,7 @@
 echo "stopping containers..."
-docker-compose -f ./srcs/docker-compose.yml down
+sudo docker-compose -f ./srcs/docker-compose.yml down
 echo "removing volumes..."
-if [ "$(docker volume ls -q)" != "" ]
+if [ "$(sudo docker volume ls -q)" != "" ]
 then
-  docker volume rm -f $(docker volume ls -q)
+  sudo docker volume rm -f $(sudo docker volume ls -q)
 fi
--- Inception/srcs/requirements/tools/fclean.sh
+++ Inception/srcs/requirements/tools/fclean.sh
@@ -1,9 +1,9 @@
 echo "stopping containers..."
-docker-compose -f ./srcs/docker-compose.yml down
+sudo docker-compose -f ./srcs/docker-compose.yml down
 echo "removing volumes..."
-if [ "$(docker volume ls -q)" != "" ]
+if [ "$(sudo docker volume ls -q)" != "" ]
 then
-  docker volume rm -f $(docker volume ls -q)
+  sudo docker volume rm -f $(sudo docker volume ls -q)
 fi
 echo "cleaning hosts..."
 sed '/mpaterno.42.fr/d' /etc/hosts > temp && sudo mv temp /etc/hosts
